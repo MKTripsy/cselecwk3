fc33183d65735e4d84426fb2bd4c4479c7a83845
MKTripsy <janchristiansentripolca@gmail.com>
2025-05-24 20:53:23 +0800

    first commit

---
+++ app/dashboard/contactus/page.tsx
@@ -0,0 +1,3 @@
+export default function Invoices() {
+  return <p>Contact Us Page</p>;
+}
---
+++ app/layout.tsx
@@ -0,0 +1,29 @@
+// export default function RootLayout({
+//   children,
+// }: {
+//   children: React.ReactNode;
+// }) {
+//   return (
+//     <html lang="en">
+//       <body>{children}</body>
+//     </html>
+//   );
+// }
+
+import '@/app/ui/global.css';
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body>
+        <h1>Header</h1>
+        {children}
+        <h1>Footer</h1>
+        </body>
+    </html>
+  );
+}
